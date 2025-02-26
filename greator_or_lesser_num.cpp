cf049e97a647752d7b91bf928ce00302b237c434
priyanshi585 <128904363+priyanshi585@users.noreply.github.com>
2025-02-26 21:32:58 +0530

    Create greator_or_lesser_num.cpp

---
+++ greator_or_lesser_num.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a , b;
+    cout<<"Enter two numbers:"<<endl;
+    cin>>a >>b;
+    if(a>b){
+        cout<<"First Number entered is greator than second."<<endl;
+    }
+    else{
+        cout<<"Second number is greator than first number."<<endl;
+    }
+
+    return 0;
+}
