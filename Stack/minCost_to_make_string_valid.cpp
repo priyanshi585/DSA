e410409d0ac69d0664c606d3e016b3e2ad326815
Priyanshi Goyal <128904363+priyanshi585@users.noreply.github.com>
2025-08-13 20:44:42 +0530

    Update minCost_to_make_string_valid.cpp

--- Stack/minCost_to_make_string_valid.cpp
+++ Stack/minCost_to_make_string_valid.cpp
@@ -9,7 +9,7 @@ int findMinimumCost(string &str){
         return -1;
     }
 
-    stack<char> s;
+    stack<char> s;  
     for(int i=0;i<str.length() ; i++){
         char ch = str[i] ;
         if(ch == '{' ){
