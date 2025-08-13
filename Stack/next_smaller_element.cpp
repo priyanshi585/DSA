743a7745cb96ec4549c64e716677af34abb983a6
Priyanshi Goyal <128904363+priyanshi585@users.noreply.github.com>
2025-08-13 20:44:49 +0530

    Update next_smaller_element.cpp

--- Stack/next_smaller_element.cpp
+++ Stack/next_smaller_element.cpp
@@ -8,7 +8,7 @@ vector<int> nextSmallerElement(vector<int> &arr , int n){
     s.push(-1);
 
     vector<int> ans(n);
-    
+      
     for(int i = n-1 ;i>=0 ; i--){
         int curr = arr[i] ;
         while(s.top()>=curr){
