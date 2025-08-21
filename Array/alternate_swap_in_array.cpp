0b1485a3476b37ad1c4386a28101f02fe3513a7d
Priyanshi Goyal <128904363+priyanshi585@users.noreply.github.com>
2025-08-21 15:32:32 +0530

    Update alternate_swap_in_array.cpp

--- Array/alternate_swap_in_array.cpp
+++ Array/alternate_swap_in_array.cpp
@@ -3,7 +3,8 @@ using namespace std;
 
 void inputArray(int arr[], int size) 
 {
-    cout<<"Enter te array elements: "<<endl;
+    cout<<"Enter te array elements: "<<endl; 
+    
     for(int i=0;i<size;i++)
     {
         cin>>arr[i];
@@ -44,3 +45,4 @@ int main()
 
 
 }
+
