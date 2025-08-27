0c15a3ec179313389972ecf81832b04bc3dd7a91
Priyanshi Goyal <128904363+priyanshi585@users.noreply.github.com>
2025-08-27 20:40:01 +0530

    Update shortestPath_in_undirectedGraph.cpp

--- Graph/shortestPath_in_undirectedGraph.cpp
+++ Graph/shortestPath_in_undirectedGraph.cpp
@@ -11,7 +11,7 @@ vector<int> shortestpath(vector<pair<int , int>> edges , int n  , int m , int s 
     for(int i = 0;i<edges.size();i++){
         int u = edges[i].first;
         int v = edges[i].second;
-
+  
         adj[u].push_back(v);
         adj[v].push_back(u);
     }
