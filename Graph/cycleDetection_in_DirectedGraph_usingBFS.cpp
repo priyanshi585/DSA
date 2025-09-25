ea15671945f06381aa8c314963cfbf0eef65b385
Priyanshi Goyal <128904363+priyanshi585@users.noreply.github.com>
2025-09-25 09:06:19 +0530

    Update cycleDetection_in_DirectedGraph_usingBFS.cpp

--- Graph/cycleDetection_in_DirectedGraph_usingBFS.cpp
+++ Graph/cycleDetection_in_DirectedGraph_usingBFS.cpp
@@ -10,7 +10,7 @@ int cycleDetectionInDirectedGraph( int n , vector<vector<int>> &edges )
     unordered_map<int , list<int>> adj;
     for(int i = 0;i<edges.size();i++)
     {
-        int u = edges[i][0];
+        int u = edges[i][0];  
         int v = edges[i][1]; 
         adj[u].push_back(v);
     }
